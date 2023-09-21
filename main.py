3cce6ae79d5b02bbd5892b00842b2d2be88774f5
Rohit <rohit.991063@gmail.com>
2023-09-21 22:32:21 +0530

    Revert "fifth commit"
    This reverts commit e8f72a6b69a286c7d2350b4b448f71de30fe207e.

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 <<<<<<< HEAD
-<<<<<<< HEAD
 print("I'm learning DevOps with zaid")
 =======
 >>>>>>> parent of 60bd37f (second commit)
-=======
-print("I'm learning DevOps")
->>>>>>> parent of 301dd7d (third commit)
